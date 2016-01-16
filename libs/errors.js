c4b1a10210d3374b0c9c0ea38131298649aadfac
vlados-a <azarkovlad@gmail.com>
2016-01-16 15:55:01 +0300

    Add http error class

---
+++ libs/errors.js
@@ -0,0 +1,13 @@
+var http = require('http'),
+	util = require('util');
+
+function HttpError(status, message){
+	Error.apply(this, arguments);
+	this.status = status || 500;
+	this.message = message || http.STATUS_CODES[this.status] || "Error";
+}
+
+util.inherits(HttpError, Error);
+HttpError.prototype.name = 'HttpError';
+
+exports.HttpError = HttpError;
