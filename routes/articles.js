c95042a021343b6438a28663d5fa5dd3e2816be3
= <=>
2016-01-23 16:28:05 +0300

    Implement simple articles search
    Add fixes to simple articles search

--- routes/articles.js
+++ routes/articles.js
@@ -11,5 +11,30 @@ router.get('/',function(req,res,next){
         });
     })
 });
+router.post('/',function(req,res,next){
+    var query = {};
+    if(req.body.title !== '') query.title = req.body.title;
+    Article.find(query).populate('creator')
+    .exec(function(err, articles){
+        if(err) return next(err);
+        if(req.body.author === ''){
+            res.render("articles",{
+                articles: articles
+            });
+        }
+        else{
+            var a = [];
+            for(var i = 0, l = articles.length; i < l; i++){
+                if(articles[i].creator.username == req.body.author){
+                    a.push(articles[i]);
+                }
+            }
+            res.render("articles", {
+                articles: articles
+            });
+        }
+
+    });
+});
 
 module.exports = router;
